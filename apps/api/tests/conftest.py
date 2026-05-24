ffc2c97f165c0a1b3e5ea919d7643afd5cd6f02c
Mark Fox <33203865+mark-fox@users.noreply.github.com>
2026-05-24 17:19:55 -0500

    clear products before each test

--- apps/api/tests/conftest.py
+++ apps/api/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
+from app.db.session import SessionLocal
+from app.models.product import Product
 
 from app.main import app
 
@@ -7,3 +9,17 @@ from app.main import app
 @pytest.fixture
 def client() -> TestClient:
     return TestClient(app)
+
+
+@pytest.fixture(autouse=True)
+def clean_products_table():
+    db = SessionLocal()
+
+    try:
+        db.query(Product).delete()
+        db.commit()
+        yield
+    finally:
+        db.query(Product).delete()
+        db.commit()
+        db.close()
